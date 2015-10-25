3cbbb593d2e831778a4656c7417f81bf176d207f
jo <mapiator@gmail.com>
2015-10-25 10:28:25 +0000

    init v1 branch - access db via psycopg2

--- logDump1090.py
+++ logDump1090.py
@@ -9,17 +9,68 @@ import itertools
 import operator
 import logging
 from operator import itemgetter
+import psycopg2
 
 
-#db = dataset.connect('sqlite:///dbradar.db')
-db = dataset.connect('postgresql://postgres:password@192.168.1.4:5432/testdb')
+
+ # drop table if exists radar3;
+ # CREATE TABLE radar3
+ # (
+ #   id serial NOT NULL,
+ #   squawk text,
+ #   flight text,
+ #   hex text,
+ #   track integer,
+ #   lon double precision,
+ #   altitude integer,
+ #   vert_rate integer,
+ #   messages integer,
+ #   validposition integer,
+ #   validtrack integer,
+ #   lat double precision,
+ #   seen integer,
+ #   speed integer,
+ #   geom geography(POINT,4326),
+ #   CONSTRAINT radar3_pkey PRIMARY KEY (id)
+ # );
+ # INSERT INTO radar3 (geom) VALUES (ST_GeographyFromText('POINT(-9.375732 38.742122)') );
 
 
-def startdump():
+def initDatabase():
     try:
-        os.system('nohup ./dump1090 --interactive --net')
-    except Exception as e:
-        print (e)
+        conn = psycopg2.connect("dbname='radar' user='postgres' host='192.168.1.4' password='password'")
+        cur = conn.cursor()
+
+        cur.execute("""
+                drop table if exists radar3;
+                 CREATE TABLE radar3
+                 (
+                   id serial NOT NULL,
+                   squawk text,
+                   flight text,
+                   hex text,
+                   track integer,
+                   lon double precision,
+                   altitude integer,
+                   vert_rate integer,
+                   messages integer,
+                   validposition integer,
+                   validtrack integer,
+                   lat double precision,
+                   seen integer,
+                   speed integer,
+                   geom geography(POINT,4326),
+                   CONSTRAINT radar3_pkey PRIMARY KEY (id)
+                 );""")
+    except:
+        print "I am unable to connect to the database"
+
+
+
+
+
+
+
 
 def get_data():
     try:
@@ -27,9 +78,10 @@ def get_data():
         while True:
             json_data = requests.get('http://192.168.1.4:8080/data.json')
             data = json_data.json()
+            print data
             for each in data:
                 stream.append(each)
-                if len(stream) > 100:
+                if len(stream) > 10:
                     DuplicateRemover(stream)
                     stream = []
                     get_data()
@@ -39,19 +91,19 @@ def get_data():
         pass
 
 def DuplicateRemover(stream):
-    ## http://stackoverflow.com/questions/11092511/python-list-of-unique-dictionaries
-    #cleanStream = {v['flight']['lon']['lat']: v for v in stream}.values()
-    ##solution found for de-deduplicate , dont fylly understand but seems to wrk
-    ##http://stackoverflow.com/questions/7090758/python-remove-duplicate-dictionaries-from-a-list/7091256#7091256
+    #  http://stackoverflow.com/questions/11092511/python-list-of-unique-dictionaries
+    # cleanStream = {v['flight']['lon']['lat']: v for v in stream}.values()
+    # solution found for de-deduplicate , dont fylly understand but seems to wrk
+    # http://stackoverflow.com/questions/7090758/python-remove-duplicate-dictionaries-from-a-list/70912567091256
 
     cleanStream = []
-    #print(stream)
+    print(stream)
     getvals = operator.itemgetter('flight','lat','lon')
     stream.sort(key=getvals)
     for k, g in itertools.groupby(stream,getvals):
         cleanStream.append(g.next())
     stream[:] = cleanStream
-    #print(cleanStream)
+    print(cleanStream)
     persist_data(cleanStream)
 
 def persist_data(data):
@@ -61,11 +113,11 @@ def persist_data(data):
             for each in data:
                 if each['flight'] is not '' and (each['lat'] > 0):
                     cnt+=1
-                    #print(each)
-                    tx['radar'].insert(dict(each))
+                    print(each)
+                    tx['radar2'].insert(dict(each))
         ts = time.time()
         st = datetime.datetime.fromtimestamp(ts).strftime('%Y-%m-%d %H:%M:%S')
-        print(str(st)+ ' ' + str(cnt)+' values inserted')
+        print(str(st) + ' ' + str(cnt)+' values inserted')
         logilog(str(cnt))
     except Exception as e:
         print(e)
@@ -76,4 +128,5 @@ def logilog(message):
 
 
 if __name__ == '__main__':
+    initDatabase()
     get_data()
